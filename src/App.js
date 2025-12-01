9fe8fca890414a38be08fa73e96b6cad59d2e1f5
Степан <stepancat61@gmail.com>
2025-12-01 08:09:59 +0300

    Revert "imperative style"

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
-	// Данный подход являеться императивным
-	const date = new Date();
-	const App = document.createElement('div');
-	App.className = 'App';
-
-	const Header = document.createElement('header');
-	Header.className = 'App-header';
-	const Logo = document.createElement('img');
-	Logo.src = logo;
-	Logo.className = 'App-logo';
-	Logo.alt = 'logo';
-
-	const Title = document.createElement('p');
-	Title.textContent = date.getFullYear();
-
-	Header.append(Logo);
-	Header.append(Title);
-	App.append(Header);
-
-	return App;
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 };
