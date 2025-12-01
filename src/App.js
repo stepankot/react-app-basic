376db533bd1d90d913d172fe3023607b2c0c7836
Степан <stepancat61@gmail.com>
2025-12-01 08:16:17 +0300

    declarative style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 export const App = () => {
+	const date = new Date();
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -9,14 +10,7 @@ export const App = () => {
 				<p>
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+				<p className="App-link">{date.getFullYear()}</p>
 			</header>
 		</div>
 	);
